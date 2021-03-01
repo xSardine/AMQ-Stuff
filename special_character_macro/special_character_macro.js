cc7eed8592740ba5b8db003a40c0c2adc6812cba
xSardine <79758127+xSardine@users.noreply.github.com>
2021-03-01 13:40:17 +0100

    add special character macro script

---
+++ special_character_macro/special_character_macro.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         Fast @
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  press a key and @someone
+// @match        https://animemusicquiz.com/*
+// @grant        none
+// ==/UserScript==
+
+
+
+
+function doc_keyDown(event) {
+    if (quiz.inQuiz || lobby.inLobby) {
+        if ((event.keyCode == '97') && (event.ctrlKey)) {
+            document.getElementById('gcInput').value += "@husahusahusahusa";
+        }
+        if (event.keyCode == '113') {
+            if (quiz.answerInput.inFocus) {
+                document.getElementById('qpAnswerInput').value += "ō";
+            }
+            else {
+                document.getElementById('gcInput').value += "ō";
+            }
+        }
+        if (event.keyCode == '115') {
+            if (quiz.answerInput.inFocus) {
+                document.getElementById('qpAnswerInput').value += "ū";
+            }
+            else {
+                document.getElementById('gcInput').value += "ū";
+            }
+        }
+    }
+}
+
+document.addEventListener('keydown', doc_keyDown, false);
+
+function sendChatMessage(message) {
+    gameChat.$chatInputField.val(message);
+    gameChat.sendMessage();
+}
